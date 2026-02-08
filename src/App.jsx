278d8e08d6a6c8f369d424fb70dafa63d71a5517
AminaYassin <amina6012016@gmail.com>
2026-02-08 16:24:12 +0100

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/headerSection/header";
+import Hero from "./components/heroSection/hero";
 
 function App() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/heroSection/hero/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+import ContentHero from "../contentHero";
+
+function Hero() {
+  return (
+    <div className={styles.container}>
+      <ContentHero />
+    </div>
+  );
+}
+export default Hero;
