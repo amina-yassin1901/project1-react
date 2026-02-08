a2bb8e199de4488c7bb0280719633d590a3f7cc9
AminaYassin <amina6012016@gmail.com>
2026-02-08 15:15:45 +0100

    fix icons

--- src/components/headerSection/header/index.jsx
+++ src/components/headerSection/header/index.jsx
@@ -3,14 +3,18 @@ import NavMenu from "../navMenu";
 import styles from "./styles.module.css";
 import Links from "../links";
 import Btn from "../btn";
+import facebookIcon from "/src/assets/icons/facebook.svg";
+import telegramIcon from "/src/assets/icons/telegram.svg";
+import twitterIcon from "/src/assets/icons/twitter.svg";
+import mediumIcon from "/src/assets/icons/medium.svg";
 
 function Header() {
   const navMenuValue = ["Home", "Blog", "Features", "Pricing", "Documentation"];
   const linksItems = [
-    { id: 1, label: "Facebook", icon: "../../assets/icons/facebook.svg" },
-    { id: 2, label: "Telegram", icon: "../../assets/icons/telegram.svg" },
-    { id: 3, label: "Twitter", icon: "../../assets/icons/twitter.svg" },
-    { id: 4, label: "Medium", icon: "../../assets/icons/medium.svg" },
+    { id: 1, label: "Facebook", icon: facebookIcon },
+    { id: 2, label: "Telegram", icon: telegramIcon },
+    { id: 3, label: "Twitter", icon: twitterIcon },
+    { id: 4, label: "Medium", icon: mediumIcon },
   ];
   return (
     <header className={styles.header}>
